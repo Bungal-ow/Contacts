c38699236bac9b8a9b0eab2114076505ad83f9fc
dnkaneda <ryqz.ig@gmail.com>
2020-03-12 14:56:01 -0700

    Employ faker.js to create Agents for db seed

--- database/index.js
+++ database/index.js
@@ -1,9 +1,46 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost:27017/abode_dh', { useNewUrlParser: true });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  // all code goes here
+const propertySchema = mongoose.Schema({
+  address: String,
+  numBd: Number,
+  numBa: Number,
+  sqft: Number,
+  availableOn: Date,
+  marketValEst: Number,
+  listedby: [{
+    isAgency: Boolean,
+    isAgent: Boolean,
+    representative_id: mongoose.Schema.Types.ObjectId,
+  }],
 });
+
+const agencySchema = mongoose.Schema({
+  name: String,
+  address: String,
+  agents: [mongoose.Schema.Types.ObjectId],
+});
+
+const agentSchema = mongoose.Schema({
+  name: String,
+  title: String,
+  rating: Number,
+  numSales: Number,
+  phoneNum: String,
+  bookings: [{
+    date: String,
+    timeblock: Number,
+  }],
+});
+
+const Property = mongoose.model('Property', propertySchema);
+const Agency = mongoose.model('Agency', agencySchema);
+const Agent = mongoose.model('Agent', agentSchema);
+
+module.exports = {
+  Property,
+  Agency,
+  Agent,
+};
