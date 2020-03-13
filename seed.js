cf664190450e190d49ed8ef41995cf09849a9e95
dnkaneda <ryqz.ig@gmail.com>
2020-03-12 19:58:08 -0700

    Refactor agent seeding scripts

--- seed.js
+++ seed.js
@@ -2,32 +2,14 @@
 const faker = require('faker');
 const { Agent, Property } = require('./database/index.js');
 
-const generateRandomNum = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+const generateRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const seedSellersAgents = () => {
-  const quantity = 1;
+const seedAgents = (quantity, title) => {
   const agents = [];
   for (let i = 0; i < quantity; i += 1) {
     agents.push(new Agent({
       name: faker.name.findName(),
-      title: 'Seller\'s Agent',
-      rating: generateRandomNum(3, 5),
-      numSales: generateRandomNum(0, 30),
-      phoneNum: faker.phone.phoneNumber(),
-    }));
-  }
-  return agents;
-};
-
-const seedPremierAgents = () => {
-  const quantity = generateRandomNum(2, 3);
-  const agents = [];
-  for (let i = 0; i < quantity; i += 1) {
-    agents.push(new Agent({
-      name: faker.name.findName(),
-      title: 'Premier Agent',
+      title,
       rating: generateRandomNum(3, 5),
       numSales: generateRandomNum(0, 30),
       phoneNum: faker.phone.phoneNumber(),
@@ -44,8 +26,8 @@ const seedProperties = () => {
   const oneMonth = new Date();
   oneMonth.setMonth(now.getMonth() + 1);
 
-  const sellersAgent = seedSellersAgents();
-  const premierAgent = seedPremierAgents();
+  const sellersAgent = seedAgents(1, 'Seller\'s Agent');
+  const premierAgent = seedAgents(3, 'Premier Agent');
 
   for (let i = 0; i < quantity; i += 1) {
     properties.push(new Property({
@@ -54,17 +36,15 @@ const seedProperties = () => {
         ${faker.address.zipCode()}`,
       numBd: generateRandomNum(1, 5),
       numBa: generateRandomNum(1, 5),
-      sqft: generateRandomNum(10, 30) * 100,x
+      sqft: generateRandomNum(10, 30) * 100,
       availableOn: faker.date.between(now, oneMonth),
       marketValEst: generateRandomNum(500000, 4000000),
-      listedby: sellersAgent.concat(premierAgent),
+      contact: sellersAgent.concat(premierAgent),
     }));
   }
   return properties;
 };
 
 module.exports = {
-  seedSellersAgents,
-  seedPremierAgents,
   seedProperties,
 };
