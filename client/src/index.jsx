da60740510729393d764efce2e780494a19e7b08
Dennis Huynh <dhkaneda@dnsh-MBP.local>
2020-03-17 09:46:08 -0700

    Define Summary with dummy data

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Summary from './components/Summary';
 
-const title = 'React with Webpack and Babel YAY';
 ReactDOM.render(
-  <div>{title}</div>,
+  <Summary />,
   document.getElementById('app'),
 );
