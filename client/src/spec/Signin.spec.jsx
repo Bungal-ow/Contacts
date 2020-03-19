3ed3eac4f3b1161ec89e50005bc31b40d4e442be
dhkaneda <ryqz.ig@gmail.com>
2020-03-19 13:00:03 -0700

    Setup spec files for signin/share modals

---
+++ client/src/spec/Signin.spec.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow } from 'enzyme';
+import Signin from '../components/Signin';
+
+describe('Signin modal', () => {
+  xit('displays one form', () => {
+  });
+});
