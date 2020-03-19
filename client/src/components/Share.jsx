e9f345938f2b89159ce2cd63372b6c3cfa020834
dhkaneda <ryqz.ig@gmail.com>
2020-03-19 11:56:59 -0700

    Add inputs for share modal

--- client/src/components/Share.jsx
+++ client/src/components/Share.jsx
@@ -1,14 +1,31 @@
+/* eslint-disable no-alert */
 import React, { Component } from 'react';
 
 class Share extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      recipientEmail: '',
+      userEmail: '',
+      userMessage: '',
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    const { recipientEmail, userEmail, userMessage } = this.state;
+    alert(`Submitted ${recipientEmail}, ${userEmail}, ${userMessage}`);
+    event.preventDefault();
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
   }
 
   render() {
+    const { recipientEmail, userEmail, userMessage } = this.state;
     return (
       <div>
         <div className="header">
@@ -16,7 +33,37 @@ class Share extends Component {
         </div>
         <div className="body">
           <div className="form">
-            form
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                recipient&apos;s email
+                <input
+                  name="recipientEmail"
+                  type="email"
+                  defaultValue={recipientEmail}
+                  onChange={this.handleChange}
+                />
+                <br></br>
+              </label>
+              <label>
+                your email
+                <input
+                  name="userEmail"
+                  type="email"
+                  defaultValue={userEmail}
+                  onChange={this.handleChange}
+                />
+                <br></br>
+              </label>
+              <label>
+                message (optional)
+                <textarea
+                  name="userMessage"
+                  defaultValue={userMessage}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
           </div>
         </div>
       </div>
