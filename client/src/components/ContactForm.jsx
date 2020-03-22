67361dc6db36025b5257e494be409bade63a47c0
dhkaneda <ryqz.ig@gmail.com>
2020-03-22 12:33:36 -0700

    Styling fixes

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -3,11 +3,11 @@
 import React from 'react';
 
 const legalBlock = (
-  `By pressing Contact Agent, you agree that Zillow Group and real estate professionals
+  `By pressing Contact Agent, you agree that Abode Group and real estate professionals
   may call/text you about your inquiry, which may involve use of automated means
   and prerecorded/artificial voices. You don't need to consent as a condition of
   buying any property, goods or services. Message/data rates may apply. You also
-  agree to our Terms of Use. Zillow does not endorse any real estate professionals.`
+  agree to our Terms of Use. Abode does not endorse any real estate professionals.`
 );
 
 const ContactForm = ({ handleSubmit, handleChange, defaultMessage }) => (
@@ -44,7 +44,7 @@ const ContactForm = ({ handleSubmit, handleChange, defaultMessage }) => (
         onChange={handleChange}
       />
       <br></br>
-      <input type="submit" name="Contact agent" />
+      <input id="contact-form-submit" type="submit" value="Contact agent" />
       <div>
         <p className="legal">
           {legalBlock}
